866eba2eae3cf20eb9d44e25706dac85a5df941a
Guillaume Tucker <guillaume.tucker@collabora.com>
2018-02-15 10:30:40 +0000

    initial commit: kci_add_lab and sample settings
    Add kci_add_lab script to create a lab entry via the KernelCI API, and
    use a _settings module to keep site-specific configuration data.
    
    A _sample_settings.py file is given as a template, the real
    _settings.py should not be kept under source control in a public
    repository (listed in .gitignore).
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ _sample_settings.py
@@ -0,0 +1,12 @@
+# Rename to _settings.py and adjust with real data.
+
+HOSTS = {
+    'localhost': {
+        'url': 'http://localhost:8888',
+        'token': '1234-5678',
+    },
+    'other-host': {
+        'url': 'https://other-host.org',
+        'token': '1234-5678',
+    },
+}
